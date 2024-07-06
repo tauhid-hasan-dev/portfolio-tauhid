6beb2ce73f1eb322be562362b5741c71356af067
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2024-07-06 22:36:09 +0300

    Adding nav item

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,18 +5,24 @@ import { Button } from "./ui/button";
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white">
-      <div className="container mx-auto">
+      <div className="container mx-auto flex justify-between items-center">
         <Link href="/">
           <h1 className="text-4xl font-semibold">
             tasan<span className="text-accent">.</span>
           </h1>
         </Link>
-        <div className="hidden xl:flex">
+
+        {/*  desktop nav */}
+        <div className="hidden xl:flex items-center gap-8">
           <Nav />
           <Link href="/contact">
             <Button>Hire me</Button>
           </Link>
         </div>
+
+        {/* mobile nav */}
+
+        <div className="xl:hidden">mobile nav</div>
       </div>
     </header>
   );
