90bcd3f33abee8e489342586b3c5d0ef75d90f43
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2024-07-06 21:30:16 +0300

    navbar added

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,25 @@
-
-
-import React from "react";
+import Link from "next/link";
+import Nav from "./Nav";
+import { Button } from "./ui/button";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto">
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            tasan<span className="text-accent">.</span>
+          </h1>
+        </Link>
+        <div className="hidden xl:flex">
+          <Nav />
+          <Link href="/contact">
+            <Button>Hire me</Button>
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
