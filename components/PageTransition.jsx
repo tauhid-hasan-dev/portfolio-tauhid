c499383df8cd38ce26a860a02a66f0306b2adf41
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2024-07-07 12:04:56 +0300

    delay added

--- components/PageTransition.jsx
+++ components/PageTransition.jsx
@@ -1,10 +1,25 @@
 "use client";
 
-import React from "react";
-/* import AnimatePresence from "framer-motion"; */
+import { AnimatePresence, motion } from "framer-motion";
+import { usePathname } from "next/navigation";
 
 const PageTransition = ({ children }) => {
-  return <div>{children}</div>;
+  const pathname = usePathname();
+  return (
+    <AnimatePresence>
+      <div key={pathname}>
+        <motion.div
+          initial={{ opacity: 1 }}
+          animate={{
+            opacity: 0,
+            transition: { delay: 0.3, duration: 0.2, ease: "easeInOut" },
+          }}
+          className="h-screen w-screen fixed bg-primary top-0 pointer-events-none"
+        />
+        {children}
+      </div>
+    </AnimatePresence>
+  );
 };
 
 export default PageTransition;
