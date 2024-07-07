d6a15f05f862b78b013d20eac96f1e3d852699ae
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2024-07-07 11:57:06 +0300

    page trasition added

---
+++ components/PageTransition.jsx
@@ -0,0 +1,10 @@
+"use client";
+
+import React from "react";
+/* import AnimatePresence from "framer-motion"; */
+
+const PageTransition = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+export default PageTransition;
