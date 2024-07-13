ab6ab3ffb08d82c947ba04ebd3786af9282ba1c6
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2024-07-14 00:36:51 +0300

    updated

--- components/Stats.jsx
+++ components/Stats.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+"use client";
+
+import CountUp from "react-countup";
+
+const stats = [
+  {
+    num: 2.5,
+    text: "Years of Experiences",
+  },
+  {
+    num: 10,
+    text: "Project Completed",
+  },
+  {
+    num: 8,
+    text: "Technologies ",
+  },
+  {
+    num: 22,
+    text: "Country Visited ",
+  },
+];
 
 const Stats = () => {
-  return <div>Stats</div>;
+  return (
+    <section className="pt-4 pb-12 xl:pt-0 xl:pb-0">
+      <div className="container mx-auto">
+        <div className="flex flex-wrap gap-6 max-w-[80vw] mx-auto xl:max-w-none">
+          {stats.map((item, index) => {
+            return (
+              <div
+                className="flex-1 flex gap-4 items-center justify-center xl:justify-start"
+                key={index}
+              >
+                <CountUp
+                  end={item.num}
+                  duration={5}
+                  delay={2}
+                  className="text-4xl xl:text-6xl font-extrabold "
+                />
+                <p
+                  className={`${
+                    item.text.length > 15 ? "max-w-[100px]" : "max-w-[150px]"
+                  } leading-snug text-white/50`}
+                >
+                  {item.text}
+                </p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Stats;
