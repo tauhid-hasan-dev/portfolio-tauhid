1d54d17628c6613fc7db385bf2ed9f08e704a2f7
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2024-07-13 14:59:25 +0300

    mobile navbar is working

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -36,7 +36,26 @@ const MobileNav = () => {
         <CiMenuFries className="text-[32px] text-accent" />
       </SheetTrigger>
       <SheetContent className="flex flex-col">
-        <div>logo</div>
+        <div className="mt-15 mb-40 text-center text-2xl">
+          <Link href="/">
+            <h1>
+              tasan<span className="text-accent">.</span>
+            </h1>
+          </Link>
+        </div>
+        <nav className="flex flex-col justify-center items-center gap-8">
+          {links.map((link, index) => (
+            <Link
+              key={index}
+              href={link.path}
+              className={`${
+                link.path === pathname && "text-accent border-b-2 border-accent"
+              } text-xl capitalize hover:text-accent transition-all`}
+            >
+              {link.name}
+            </Link>
+          ))}
+        </nav>
       </SheetContent>
     </Sheet>
   );
