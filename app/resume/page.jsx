072392ef09e13e895e097f8e059a5bbe4b3f7c45
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2024-07-14 18:59:41 +0300

    style udpated

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -164,10 +164,13 @@ const Resume = () => {
                           key={index}
                           className="bg-[#232329] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1"
                         >
-                          <span>{item.duration}</span>
-                          <h3>{item.position}</h3>
-                          <div>
-                            <p>{item.company}</p>
+                          <span className="text-accent">{item.duration}</span>
+                          <h3 className="text-xl max-w-[268px] min-h-[60px] text-center lg:text-left">
+                            {item.position}
+                          </h3>
+                          <div className="flex items-center gap-3">
+                            <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
+                            <p className="text-white/50">{item.company}</p>
                           </div>
                         </li>
                       );
