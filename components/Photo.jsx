2936c5f4e02d256ab74a4df909f6b43d105c440c
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2024-07-14 12:20:44 +0300

    services udpated

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -10,14 +10,14 @@ const Photo = () => {
         initial={{ opacity: 0 }}
         animate={{
           opacity: 1,
-          transition: { delay: 2, duration: 0.3, ease: "easeIn" },
+          transition: { delay: 0.6, duration: 0.3, ease: "easeIn" },
         }}
       >
         <motion.div
           initial={{ opacity: 0 }}
           animate={{
             opacity: 1,
-            transition: { delay: 2.4, duration: 0.3, ease: "easeInOut" },
+            transition: { delay: 0.6, duration: 0.3, ease: "easeInOut" },
           }}
           className="w-[298px] h-[298px] xl:w-[450px] xl:h-[450px] mix-blend-lighten "
         >
