04a5f5f4e4fe512dbc31d385a9f277aab1828513
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2024-07-15 23:11:13 +0300

    work page updated

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,7 +1,80 @@
-import React from "react";
+"use client";
+
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+import link from "next/link";
+import Image from "next/image";
+
+const projects = [
+  {
+    num: "01",
+    category: "frontend",
+    title: "project 1",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate magnam modi.",
+    stack: [{ name: "Html 5" }, { name: "Css 3" }, { name: "Javascript" }],
+    image: "/assets/work/thumb1.png",
+    live: "",
+    github: "",
+  },
+  {
+    num: "02",
+    category: "FullStack",
+    title: "project 2",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate magnam modi.",
+    stack: [{ name: "NextJs" }, { name: "Tailwind CSS" }, { name: "NodeJs" }],
+    image: "/assets/work/thumb2.png",
+    live: "",
+    github: "",
+  },
+  {
+    num: "03",
+    category: "frontend",
+    title: "project 3",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate magnam modi.",
+    stack: [{ name: "NextJs" }, { name: "Tailwind CSS" }, { name: "NodeJs" }],
+    image: "/assets/work/thumb3.png",
+    live: "",
+    github: "",
+  },
+];
 
 const Work = () => {
-  return <div>Work</div>;
+  const [project, setProject] = useState(projects[0]);
+
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+          <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+            <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
+              <div>{project.num}</div>
+            </div>
+            <h2 className="text-[42px] font-bold leading-none">
+              {project.category}
+            </h2>
+          </div>
+          <div className="w-full xl:w-[50%] ">slider</div>
+        </div>
+      </div>
+    </motion.section>
+  );
 };
 
 export default Work;
