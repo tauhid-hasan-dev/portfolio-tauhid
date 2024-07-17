c6bb41f273f4b1386d438260bea3ea97d98f43db
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2024-07-17 10:38:27 +0300

    slide is working

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -12,7 +12,8 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 
-import link from "next/link";
+import WorkSliderBtn from "../../components/WorkSliderBtns";
+
 import Image from "next/image";
 import Link from "next/link";
 
@@ -63,7 +64,10 @@ const Work = () => {
   return (
     <motion.section
       initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 0.4, duration: 0.4, ease: "easeIn" },
+      }}
       className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
     >
       <div className="container mx-auto">
@@ -133,7 +137,7 @@ const Work = () => {
                 return (
                   <SwiperSlide key={index} className="w-full">
                     <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
-                      <div></div>
+                      <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
                       <div className="relative w-full h-full">
                         <Image
                           src={project.image}
@@ -146,6 +150,11 @@ const Work = () => {
                   </SwiperSlide>
                 );
               })}
+              {/*      slider button */}
+              <WorkSliderBtn
+                containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
+                btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all"
+              />
             </Swiper>
           </div>
         </div>
