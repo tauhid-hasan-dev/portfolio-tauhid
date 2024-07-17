49702d1c0cfc54f671b3b4637f0973d88092434a
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2024-07-17 10:11:19 +0300

    slide is working

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -55,6 +55,11 @@ const projects = [
 const Work = () => {
   const [project, setProject] = useState(projects[0]);
 
+  const handleSlideChange = (swiper) => {
+    const currentindex = swiper.activeIndex;
+    setProject(projects[currentindex])
+  }
+
   return (
     <motion.section
       initial={{ opacity: 0 }}
@@ -117,7 +122,18 @@ const Work = () => {
               </div>
             </div>
           </div>
-          <div className="w-full xl:w-[50%] ">slider</div>
+          <div className="w-full xl:w-[50%]">
+            <Swiper
+              spaceBetween={30}
+              slidesPerView={1}
+              className="xl:h-[520px] mb-12"
+              onSlideChange={handleSlideChange}
+            >
+              {projects.map((project, index) => {
+                return <SwiperSlide key={index}>Slide</SwiperSlide>;
+              })}
+            </Swiper>
+          </div>
         </div>
       </div>
     </motion.section>
