112e11ebb669db317f16ea19cc3c7eeb1c9fdd27
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2024-07-17 14:40:52 +0300

    contact page working perfectly

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -16,7 +16,7 @@ import {
 
 import { motion } from "framer-motion";
 
-import { FaPhoneAlt, FaEnvelope, FapMapMarkerAlt } from "react-icons/fa";
+import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
 
 const info = [
   {
@@ -25,12 +25,12 @@ const info = [
     description: "(+90) 507 626 0228",
   },
   {
-    icon: <FaPhoneAlt />,
+    icon: <FaEnvelope />,
     title: "Email",
     description: "tauhidhasan11@gmail.com",
   },
   {
-    icon: <FapMapMarkerAlt />,
+    icon: <FaMapMarkerAlt />,
     title: "Address",
     description: "Istanbul, Türkiye",
   },
@@ -42,16 +42,16 @@ const Contact = () => {
       initial={{ opacity: 0 }}
       animate={{
         opacity: 1,
-        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+        transition: { delay: 0.4, duration: 0.4, ease: "easeIn" },
       }}
       className="py-6"
     >
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-[30px]">
           {/* form */}
-          <div className="xl:h-[54%] order-2 xl:order-none">
+          <div className="xl:w-[54%] order-2 xl:order-none">
             <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-              <h3 className="text-4xl text-accent">Lets work together</h3>
+              <h3 className="text-4xl text-accent">Let's work together</h3>
               <p className="text-white/60">
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum
                 nihil sapiente pariatur id totam.
@@ -80,10 +80,27 @@ const Contact = () => {
                 className="h-[200px]"
                 placeholder="Type your message here"
               />
+              <Button size="md" className="max-w-40">
+                Send Message
+              </Button>
             </form>
           </div>
           {/* info */}
-          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0"></div>
+          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
+            <ul className="flex flex-col gap-10">
+              {info.map((item, index) => (
+                <li key={index} className="flex items-center gap-6">
+                  <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                    <div className="text-[28px]">{item.icon}</div>
+                  </div>
+                  <div className="flex-1">
+                    <p className="text-white/60">{item.title}</p>
+                    <h3 className="text-xl">{item.description}</h3>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </motion.section>
