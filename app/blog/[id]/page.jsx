1af49da222354bbbe54363d01d80aa8faa128cc4
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2024-07-18 09:27:10 +0300

    updated

--- app/blog/[id]/page.jsx
+++ app/blog/[id]/page.jsx
@@ -6,8 +6,6 @@ const BlogDetail = () => {
   const { id } = useParams();
   const [post, setPost] = useState({});
 
-  console.log(post);
-
   useEffect(() => {
     fetch(`https://portfolio-backend-one-ivory.vercel.app/api/blogs/${id}`)
       .then((response) => response.json())
@@ -16,33 +14,6 @@ const BlogDetail = () => {
       });
   }, [id]);
 
-  console.log({ id });
-
-  // Fetch the blog post details based on the ID
-  const blogPosts = [
-    {
-      id: "1",
-      date: "July 17, 2024",
-      headline: "How to use Tailwind CSS",
-      content:
-        "Tailwind CSS is a utility-first CSS framework that provides low-level utility classes to build custom designs without leaving your HTML...Tailwind CSS is a utility-first CSS framework that provides low-level utility classes to build custom designs without leaving your HTML...Tailwind CSS is a utility-first CSS framework that provides low-level utility classes to build custom designs without leaving your HTML...",
-    },
-    {
-      id: "2",
-      date: "July 16, 2024",
-      headline: "Understanding React Hooks",
-      content:
-        "React Hooks are functions that let you use state and other React features in functional components. They were introduced in React 16.8...",
-    },
-    // Add more blog posts here
-  ];
-
-  /* const post = blogPosts.find((post) => post.id === id);
-
-  if (!post) {
-    return <p>Post not found</p>;
-  }
- */
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-2xl font-semibold">{post?.headline}</h1>
